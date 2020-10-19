1d17e14aa7432656e24cdeb27895901f7b47a019
Yuyeol <uy23@naver.com>
2020-10-19 18:09:53 +0900

    add api

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
@@ -9,16 +8,21 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
+import Header from "Components/Header";
 
 export default () => (
+  // Router는 하나의 child만 갖는다.
   <Router>
-    {/* Switch는 한번에 하나의 Route만 Render하게 해줌. */}
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" component={Search} />
-      {/* 위 route이외에는 어느 페이지든 "/"로 보내준다 */}
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      {/* Switch는 한번에 하나의 Route만 Render하게 해줌. */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" component={Search} />
+        {/* 위 route이외에는 어느 페이지든 "/"로 보내준다 */}
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
